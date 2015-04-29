183df65d03b5ff720f2b543e467f884fbd337a76
daveb1034 <daveb1034@googlemail.com>
2015-04-29 20:32:28 +0100

    dictionary
    completed feature dictionary and ensured code matched the master.

--- nvgReader.py
+++ nvgReader.py
@@ -35,10 +35,25 @@ nvg = r"R:\Tasking\NVGTools\testData\nvg_1_4\APP6A_SAMPLE.nvg"
 # o or m for optioanl and mandatory
 
 # dictionary of mandatory fields for each NVG feature
-features = {'point':['x','y','symbol','modifiers','uri','label','style']}
+# <a>, <g> and <composite> features not yet implemented
+features = {'point':['x','y','symbol','modifier','uri','label','style','course',
+            'speed'],
+            'text':['content','x','y','rotation','uri','style'],
+            'multipoint':['points','symbol','modifier','uri','label','style'],
+            'circle':['cx','cy','r','uri','label','style','symbol','modifier'],
+            'ellipse':['cx','cy','rx','ry','rotation','uri','label','style',
+            'modifier','course','speed','symbol'],
+            'polyline':['points','uri','label','style','symbol','modifier'],
+            'corridor':['points','width','minaltitude','maxaltitude','uri',
+            'label','style','symbol','modifier'],
+            'polygon':['points','uri','label','style','symbol','modifier'],
+            'arc':['cx','cy','course','speed','rotation','startangle','endangle',
+            'uri','label','style','symbol','modifier'],
+            'arcband':['cx','cy','minr','maxr','startangle','endangle','uri',
+            'label','style','symbol','modifier']}
 
 class Reader(object):
-    """NATO Vector Graphic Reader instance. Reads and process a NATO Vector
+    """NATO Vector Graphic Reader instance. Reads and processes a NATO Vector
     Graphic to ESRI Geometry.
     """
     def __init__(self,nvgFile,namespaces):
@@ -49,79 +64,118 @@ class Reader(object):
         # parse the nvgFile
         self.dom = xml.dom.minidom.parse(self.nvgFile)
         # get the nvg version
+        # consider moving this to a seperate method
         self.version = self.dom.documentElement.getAttribute("version")
+
         # need to define the outputs based on the datatypes in the nvg
         self.esriPolygon = None
         self.esriPolyline = None
         self.esriPoint = None
+
         return
+
+
+    def _getElement(self,tag):
+        """Return all elements with given tag.
+        """
+        return self.dom.getElementsByTagName(tag)
+
     def _point(self):
-        """Reads all points from the given nvg document object.
+        """Reads points with parent node of nvg.
+        All points within the nvg file that have the parent node nvg are read
+        and the geometries created and stored in self.esriPoint.
+        Any point nods that are part of a composite symbol or a or g node are
+        ignored.
         """
         # get the namespace for the file. need to try with mulitple namespaces
         #print self.dom.documentElement.namespaceURI
         self.esriPoint = []
 
         # get all the points
-        points = self.dom.getElementsByTagName("point")
+        points = self._getElement("point")
 
         # extract the attributes and add to the esriPoint
         for point in points:
             newPoint = []
-            try:
-                #TODO create a point geometry object rather than returning the x,y
-                x = float(point.getAttribute('x'))
-                newPoint.append(x)
-                y = float(point.getAttribute('y'))
-                newPoint.append(y)
-                symbol = point.getAttribute('symbol').split(':')
-                newPoint.append(symbol)
-            except:
-                # if any of the mandatory attributes are missing log it and continue
-                # to next point
-                print "invalid point instance, mandatory atribute missing"
+            # only load points that are not part of composite symbols or members
+            # a or g tags. The parent node should be <nvg>
+            if not point.parentNode.nodeName == 'nvg':
                 continue
-            # get the optional attributes
-            try:
-                modifiers = point.getAttribute('modifiers')
-                if not modifiers == '':
-                    newPoint.apend(modifiers)
-                else:
-                    newPoint.append(None)
-            except:
-                newPoint.append(None)
+            else:
+                try:
+                    x = float(point.getAttribute('x'))
+                    y = float(point.getAttribute('y'))
 
-            try:
-                uri = point.getAttribute('uri')
-                if not uri == '':
-                    newPoint.append(uri)
-                else:
+                    # create the point geometry
+                    pnt = arcpy.Point(x,y)
+                    pntGeom = arcpy.PointGeometry(pnt)
+                    newPoint.append(pntGeom)
+                    symbol = point.getAttribute('symbol').split(':')
+                    newPoint.append(symbol)
+                except:
+                    # if any of the mandatory attributes are missing log it and continue
+                    # to next point
+                    print "invalid point instance, mandatory atribute missing"
+                    continue
+                # get the optional attributes
+                try:
+                    # spec spells it as modifiers, sample data uses modifier
+                    modifiers = point.getAttribute('modifier')
+                    if not modifiers == '':
+                        newPoint.append(modifiers)
+                    else:
+                        newPoint.append(None)
+                except:
                     newPoint.append(None)
-            except:
-                newPoint.append(None)
 
-            try:
-                label = point.getAttribute('label')
-                if not label == '':
-                    newPoint.append(label)
-                else:
+                try:
+                    uri = point.getAttribute('uri')
+                    if not uri == '':
+                        newPoint.append(uri)
+                    else:
+                        newPoint.append(None)
+                except:
                     newPoint.append(None)
-            except:
-                newPoint.append(None)
 
-            try:
-                style = point.getAttribute('style')
-                if not style == '':
-                    newPoint.append(style)
-                else:
+                try:
+                    label = point.getAttribute('label')
+                    if not label == '':
+                        newPoint.append(label)
+                    else:
+                        newPoint.append(None)
+                except:
                     newPoint.append(None)
-            except:
-                newPoint.append(None)
-            self.esriPoint.append(newPoint)
 
-        # TODO using the parent node handle points grouped by a or g tags
-        #print points[0].parentNode.nodeName
-        return
+                try:
+                    style = point.getAttribute('style')
+                    if not style == '':
+                        newPoint.append(style)
+                    else:
+                        newPoint.append(None)
+                except:
+                    newPoint.append(None)
+
+                try:
+                    course = point.getAttribute('course')
+                    if not course == '':
+                        newPoint.append(float(course))
+                    else:
+                        newPoint.append(None)
+                except:
+                    newPoint.append(None)
+
+                try:
+                    speed = point.getAttribute('speed')
+                    if not speed == '':
+                        newPoint.append(float(speed))
+                    else:
+                        newPoint.append(None)
+                except:
+                    newPoint.append(None)
+
+                self.esriPoint.append(newPoint)
+
+        return self.esriPoint
 
 if __name__ =="__main__":
     read = Reader(nvg,namespaces)
